bf81dc1db22e5fc2f79c79db2a82ab8fca636156
Andrius Mudinas <keistokas@gmail.com>
2017-08-20 15:15:10 +0100

    Added argument validation

--- src/Wikiled.Dictionary.Tests/Logic/DictionaryManagerTests.cs
+++ src/Wikiled.Dictionary.Tests/Logic/DictionaryManagerTests.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Threading;
 using Moq;
 using NUnit.Framework;
 using Wikiled.Core.Standard.Api;
+using Wikiled.Dictionary.Data;
 using Wikiled.Dictionary.Logic;
 
 namespace Wikiled.Dictionary.Tests.Logic
@@ -24,6 +26,23 @@ namespace Wikiled.Dictionary.Tests.Logic
         public void Construct()
         {
             Assert.Throws<ArgumentNullException>(() => new DictionaryManager(null));
+        }
+
+        [Test]
+        public void TranslateArguments()
+        {
+            Assert.ThrowsAsync<ArgumentNullException>(() => instance.Translate(null, CancellationToken.None));
+            TranslationRequest request = new TranslationRequest();
+            request.From = Language.English;
+            request.To = Language.Danish;
+            Assert.ThrowsAsync<ArgumentException>(() => instance.Translate(request, CancellationToken.None));
+            request.Word = "Test";
+            request.From = Language.Spanish;
+            request.To = Language.Danish;
+            Assert.ThrowsAsync<ArgumentException>(() => instance.Translate(request, CancellationToken.None));
+            request.From = Language.English;
+            request.To = Language.English;
+            Assert.ThrowsAsync<ArgumentException>(() => instance.Translate(request, CancellationToken.None));
         }
 
         private DictionaryManager CreateManager()
--- src/Wikiled.Dictionary/Logic/DictionaryManager.cs
+++ src/Wikiled.Dictionary/Logic/DictionaryManager.cs
@@ -7,8 +7,7 @@ using Wikiled.Dictionary.Data;
 
 namespace Wikiled.Dictionary.Logic
 {
-    public class DictionaryManager
-        : IDictionaryManager
+    public class DictionaryManager : IDictionaryManager
     {
         private readonly IApiClientFactory factory;
 
@@ -20,6 +19,9 @@ namespace Wikiled.Dictionary.Logic
         public async Task<ServiceResponse<TranslationResult>> Translate(TranslationRequest request, CancellationToken token)
         {
             Guard.NotNull(() => request, request);
+            Guard.IsValid(() => request, request, translationRequest => !string.IsNullOrWhiteSpace(translationRequest.Word), "Word is not specified");
+            Guard.IsValid(() => request, request, translationRequest => request.From == Language.English || request.To == Language.English, "Only from/to English is supported");
+            Guard.IsValid(() => request, request, translationRequest => request.From != request.To , "From and To can't match");
             var client = factory.GetPostClient("Api/Dictionary/Translate");
             var result = await client.Request<TranslationRequest, TranslationResult>(request, token).ConfigureAwait(false);
             return result;
